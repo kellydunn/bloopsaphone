32b2f18567905f7224c8c6a4d2392fa8778a2b57
Jeremy Stephens <viking415@gmail.com>
2009-09-18 17:31:23 -0500

    Initial commit; why's bloopsaphone-0.4 unpacked

---
+++ ext/ruby/extconf.rb
@@ -0,0 +1,14 @@
+require 'mkmf'
+require 'fileutils'
+
+$CFLAGS << " -I../../c "
+
+%w[../../c/notation.c ../../c/bloopsaphone.c ../../c/bloopsaphone.h].each do |fn|
+  abort "!! ERROR !!\n** #{fn} not found; type 'make ruby' in the top directory\n\n" \
+    unless File.exists? fn
+  FileUtils.cp(fn, ".")
+end
+
+have_library("portaudio")
+have_library("sndfile")
+create_makefile("bloops")
