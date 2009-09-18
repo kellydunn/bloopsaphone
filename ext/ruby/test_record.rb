349fecb2a5d1c3189f6838bebab2cb0ece8d02b5
Jeremy Stephens <viking415@gmail.com>
2009-09-18 18:13:56 -0500

    Forgot to add test_record

---
+++ ext/ruby/test_record.rb
@@ -0,0 +1,26 @@
+require './bloops'
+
+# the song object
+b = Bloops.new("out.wav")
+b.tempo = 320
+
+# an instrument
+saw = b.sound Bloops::SAWTOOTH
+
+# assign a track to the song
+b.tune saw, "c5 c6 b4 b5 d5 d6 e5 e6"
+
+# make it go
+b.play
+sleep 1 while !b.stopped?
+
+# a percussion
+beat = b.sound Bloops::NOISE
+beat.repeat = 0.6
+
+# assign a track to the song
+b.tune beat, "4 4 b4 4 d5 4 e5 e6"
+
+# make it go
+b.play
+sleep 1 while !b.stopped?
